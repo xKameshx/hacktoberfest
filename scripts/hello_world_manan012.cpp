c99699ee06d961b989c29dc3a680e758a666d8be
manan012 <33518498+manan012@users.noreply.github.com>
2018-10-04 19:25:44 +0530

    Create hello_world_manan012.cpp

---
+++ scripts/hello_world_manan012.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+
+// AUTHOR: Manan Goel
+// GITHUB: https://github.com/manan012
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout<<"Hello, World!";
+  return 0;
+}
